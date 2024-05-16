d152e4d28f3367e035e249a9044d5c822f1e9da0
MrPropreuuh <vincent.fougere@epsi.fr>
2024-05-16 19:20:06 +0200

    commit connect BDD

--- routes/artworks.js
+++ routes/artworks.js
@@ -4,9 +4,24 @@ const router = express.Router();
 const connectToDatabase = require('../lib/mongodb');
 
 router.get('/', async (req, res) => {
-    const db = await connectToDatabase();
-    const artworks = await db.collection('artworks').find({}).toArray();
-    res.json(artworks);
+    try {
+        const { db } = await connectToDatabase();
+        const artworks = await db.collection('artworks').find({}).toArray();
+        res.json(artworks);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching artworks', error });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { db } = await connectToDatabase();
+        const artwork = req.body;
+        await db.collection('artworks').insertOne(artwork);
+        res.status(201).json(artwork);
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding artwork', error });
+    }
 });
 
 module.exports = router;
